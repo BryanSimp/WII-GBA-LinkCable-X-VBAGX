125a25869861af326326bd53c9556f290ad7b8d1
Bryan Simpson <simpsonbm04@outlook.com>
2025-02-20 20:34:19 -0600

    Add files via upload

---
+++ gba/build/libSave.d
@@ -0,0 +1,7 @@
+libSave.o: C:/devkitPro/WII_GBA_Link_Cable_Dumper/gba/source/libSave.c \
+ C:/devkitPro/libgba/include/gba_dma.h \
+ C:/devkitPro/libgba/include/gba_base.h \
+ C:/devkitPro/libgba/include/gba_types.h
+C:/devkitPro/libgba/include/gba_dma.h:
+C:/devkitPro/libgba/include/gba_base.h:
+C:/devkitPro/libgba/include/gba_types.h:
---
+++ gba/build/main.d
@@ -0,0 +1,35 @@
+main.o: C:/devkitPro/WII_GBA_Link_Cable_Dumper/gba/source/main.c \
+ C:/devkitPro/libgba/include/gba.h \
+ C:/devkitPro/libgba/include/gba_affine.h \
+ C:/devkitPro/libgba/include/gba_base.h \
+ C:/devkitPro/libgba/include/gba_types.h \
+ C:/devkitPro/libgba/include/gba_compression.h \
+ C:/devkitPro/libgba/include/gba_console.h \
+ C:/devkitPro/libgba/include/gba_dma.h \
+ C:/devkitPro/libgba/include/gba_input.h \
+ C:/devkitPro/libgba/include/gba_interrupt.h \
+ C:/devkitPro/libgba/include/gba_multiboot.h \
+ C:/devkitPro/libgba/include/gba_sio.h \
+ C:/devkitPro/libgba/include/gba_sound.h \
+ C:/devkitPro/libgba/include/gba_sprites.h \
+ C:/devkitPro/libgba/include/gba_systemcalls.h \
+ C:/devkitPro/libgba/include/gba_timers.h \
+ C:/devkitPro/libgba/include/gba_video.h \
+ C:/devkitPro/WII_GBA_Link_Cable_Dumper/gba/source/libSave.h
+C:/devkitPro/libgba/include/gba.h:
+C:/devkitPro/libgba/include/gba_affine.h:
+C:/devkitPro/libgba/include/gba_base.h:
+C:/devkitPro/libgba/include/gba_types.h:
+C:/devkitPro/libgba/include/gba_compression.h:
+C:/devkitPro/libgba/include/gba_console.h:
+C:/devkitPro/libgba/include/gba_dma.h:
+C:/devkitPro/libgba/include/gba_input.h:
+C:/devkitPro/libgba/include/gba_interrupt.h:
+C:/devkitPro/libgba/include/gba_multiboot.h:
+C:/devkitPro/libgba/include/gba_sio.h:
+C:/devkitPro/libgba/include/gba_sound.h:
+C:/devkitPro/libgba/include/gba_sprites.h:
+C:/devkitPro/libgba/include/gba_systemcalls.h:
+C:/devkitPro/libgba/include/gba_timers.h:
+C:/devkitPro/libgba/include/gba_video.h:
+C:/devkitPro/WII_GBA_Link_Cable_Dumper/gba/source/libSave.h:
